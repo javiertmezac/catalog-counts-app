53ee709cb1cf30b75137e2ee86a7a7376eb3c4f4
Meza, Javier <javiermeza.cazarez@gmail.com>
2021-02-28 22:42:58 -0800

    formatted Saldo, and Fecha Registro

--- src/components/CatalogCount/CatalogCount.js
+++ src/components/CatalogCount/CatalogCount.js
@@ -14,7 +14,7 @@ function CatalogCount() {
           <table className="table table-hover">
             <thead className="thead-dark">
               <tr>
-                <th width="150">Fecha Registro</th>
+                <th width="350">Fecha Registro</th>
                 <th width="200">Cantidad</th>
                 <th>Detalles</th>
                 <th width="150">Saldo</th>
@@ -23,10 +23,18 @@ function CatalogCount() {
             <tbody>
               { list.map((cc) => (
                 <tr key={cc.id}>
-                  <td>{cc.registrationDate}</td>
-                  <td>{cc.amount}</td>
+                  <td>
+                    {new Intl.DateTimeFormat("es", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit"
+                    }).format(cc.registrationDate)}
+                  </td>
+                  <td>{new Intl.NumberFormat().format(cc.amount)}</td>
                   <td>{cc.details}</td>
-                  <td>{cc.total} </td>
+                  <td>
+                    {new Intl.NumberFormat().format(cc.total)}
+                  </td>
               </tr>
               )) }
             </tbody>
@@ -36,181 +44,5 @@ function CatalogCount() {
       </div>
     );
   }
-
-// function CatalogCount() {
-//     // const [ data, loading ] = Get("http://localhost:8888/cc-service/api/v1/catalog-count")
-//     // const list = data.catalogCountResponseCollection;
-//     const data = React.useMemo(
-
-//       () => [
- 
-//         {
- 
-//           col1: 'Hello',
- 
-//           col2: 'World',
- 
-//         },
- 
-//         {
- 
-//           col1: 'react-table',
- 
-//           col2: 'rocks',
- 
-//         },
- 
-//         {
- 
-//           col1: 'whatever',
- 
-//           col2: 'you want',
- 
-//         },
- 
-//       ],
- 
-//       []
- 
-//     )
- 
-  
- 
-//     const columns = React.useMemo(
- 
-//       () => [
- 
-//         {
- 
-//           Header: 'Column 1',
- 
-//           accessor: 'col1', // accessor is the "key" in the data
- 
-//         },
- 
-//         {
- 
-//           Header: 'Column 2',
- 
-//           accessor: 'col2',
- 
-//         },
- 
-//       ],
- 
-//       []
- 
-//     )
- 
-  
- 
-//     const {
- 
-//       getTableProps,
- 
-//       getTableBodyProps,
- 
-//       headerGroups,
- 
-//       rows,
- 
-//       prepareRow,
- 
-//     } = useTable({ columns, data })
- 
-  
- 
-//     return (
- 
-//       <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
- 
-//         <thead>
- 
-//           {headerGroups.map(headerGroup => (
- 
-//             <tr {...headerGroup.getHeaderGroupProps()}>
- 
-//               {headerGroup.headers.map(column => (
- 
-//                 <th
- 
-//                   {...column.getHeaderProps()}
- 
-//                   style={{
- 
-//                     borderBottom: 'solid 3px red',
- 
-//                     background: 'aliceblue',
- 
-//                     color: 'black',
- 
-//                     fontWeight: 'bold',
- 
-//                   }}
- 
-//                 >
- 
-//                   {column.render('Header')}
- 
-//                 </th>
- 
-//               ))}
- 
-//             </tr>
- 
-//           ))}
- 
-//         </thead>
- 
-//         <tbody {...getTableBodyProps()}>
- 
-//           {rows.map(row => {
- 
-//             prepareRow(row)
- 
-//             return (
- 
-//               <tr {...row.getRowProps()}>
- 
-//                 {row.cells.map(cell => {
- 
-//                   return (
- 
-//                     <td
- 
-//                       {...cell.getCellProps()}
- 
-//                       style={{
- 
-//                         padding: '10px',
- 
-//                         border: 'solid 1px gray',
- 
-//                         background: 'papayawhip',
- 
-//                       }}
- 
-//                     >
- 
-//                       {cell.render('Cell')}
- 
-//                     </td>
- 
-//                   )
- 
-//                 })}
- 
-//               </tr>
- 
-//             )
- 
-//           })}
- 
-//         </tbody>
- 
-//       </table>
- 
-//     );
-// }
 
 export default CatalogCount
